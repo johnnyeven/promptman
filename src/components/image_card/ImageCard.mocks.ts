501cedef85e770ecb641b1458733ac6869415c6d
liyiwen <johnnyeven@gmail.com>
2023-04-10 14:47:20 +0800

    feat. ImageCard

--- src/components/image_card/ImageCard.mocks.ts
+++ src/components/image_card/ImageCard.mocks.ts
@@ -2,7 +2,7 @@ import { CardType, IImageCard } from "./ImageCard";
 
 const base: IImageCard = {
     type: CardType.LORA,
-    previewImageUrl: "",
+    previewImageUrl: "https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/393713d6-c943-4c6a-7247-ad5f03583200/width=450/333323.jpeg",
     title: "hello world",
     score: 4.9,
     scoreCount: 1928313,
--- src/components/image_card/ImageCard.tsx
+++ src/components/image_card/ImageCard.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './ImageCard.module.css';
 import Link from "next/link";
 import { randomColorRGB } from '../../lib/random'
+import Image from "next/image";
+import { Avatar, Rate, Space, Typography } from "@arco-design/web-react";
+import humanFormat from 'human-format';
 
 export enum CardType {
     LORA = 'lora',
@@ -24,7 +27,29 @@ export interface IImageCard {
 const ImageCard: React.FC<IImageCard> = ({ type, previewImageUrl, title, score, scoreCount, like, comment, download, width, height }) => {
     return <div className={styles['image-card-indicator-root']} style={{ width: width + 'px' }}>
         <Link className={styles['image-card-root']} style={{ height: height + 'px', background: 'linear-gradient(' + randomColorRGB() + ' 0%, rgb(37, 38, 43) 100%)' }} href={""}>
-
+            <div>
+                <Image src={previewImageUrl} width={width} height={height} alt={title} style={{ objectFit: 'cover', objectPosition: '50%' }} />
+            </div>
+            <div className={styles['image-card-descriptor']}>
+                <div className={styles['image-card-descriptor-avatar']}>
+                    <Avatar size={40} className={styles['image-card-descriptor-avatar-image']}>J</Avatar>
+                </div>
+                <div className={styles['image-card-descriptor-panel']}>
+                    <div className={styles['image-card-descriptor-panel-title']}>
+                        <div className={styles['image-card-descriptor-panel-title-text']} style={{ flex: '1 1 0%', lineHeight: '1' }}>{title}</div>
+                    </div>
+                    <div className={styles['image-card-descriptor-panel-info']}>
+                        <div className={styles['image-card-descriptor-panel-info-score']}>
+                            <Space>
+                                <Rate readonly defaultValue={score} />
+                                <span>{humanFormat(scoreCount, {
+                                    maxDecimals: 1
+                                })}</span>
+                            </Space>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Link>
     </div>
 }
