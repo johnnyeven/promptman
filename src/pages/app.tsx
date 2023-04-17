84706f79d4de5dd7d34f8ed728491233a7fdd876
liyiwen <johnnyeven@gmail.com>
2023-04-17 16:39:06 +0800

    app

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,11 +1,55 @@
 import { NextPageWithLayout } from "./page";
 import PrimaryLayout from "@/components/layouts/primary/PrimaryLayout";
-import { Button, Grid, Input, Radio, Typography } from "@arco-design/web-react";
+import { Button, Grid, Image, Input, Radio, Typography } from "@arco-design/web-react";
 import Link from "next/link";
+import { useState } from "react";
+import { isArray } from "util";
 
 const { Row, Col } = Grid;
 
 const App: NextPageWithLayout = () => {
+    const [isWorking, setIsWorking] = useState(false);
+    const [isResult, setIsResult] = useState(false);
+    const [prompt, setPrompt] = useState('');
+    const [imageEncoded, setImageEncoded] = useState('');
+
+    const handleClick = () => {
+        getPredictResult();
+    }
+    const getPredictResult = async () => {
+        console.log(prompt);
+        setIsWorking(true);
+        setIsResult(false);
+        const res = await fetch('https://johnnyeven-prompthero-openjourney.hf.space/run/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: [
+                    prompt
+                ]
+            })
+        });
+        const data = await res.json();
+        if (data.data && isArray(data.data) && data.data.length > 0) {
+            console.log(data.data[0]);
+            setImageEncoded(data.data[0]);
+            setIsResult(true);
+        }
+        setIsWorking(false);
+    }
+    const getGenerateResult = () => {
+        if (!isWorking && isResult) {
+            return (
+                <Row gutter={20} justify="center" align="start" className="mb-12">
+                    <Col span={8}>
+                        <Image src={imageEncoded} width="100%" />
+                    </Col>
+                </Row>
+            )
+        }
+    }
     return (
         <div className="w-3/5">
             <Row>
@@ -15,7 +59,7 @@ const App: NextPageWithLayout = () => {
             </Row>
             <Row gutter={20} className="mb-12">
                 <Col span={24}>
-                    <Input className="h-10" size="large" placeholder="Type your prompt..." allowClear showWordLimit maxLength={200} />
+                    <Input className="h-10" value={prompt} onChange={(val) => { setPrompt(val) }} size="large" placeholder="Type your prompt..." allowClear showWordLimit maxLength={200} />
                 </Col>
             </Row>
             <Row>
@@ -42,9 +86,10 @@ const App: NextPageWithLayout = () => {
             </Row>
             <Row gutter={20} justify="center" align="start" className="mb-12">
                 <Col span={24}>
-                    <Button size='large' type='primary'>Generate</Button>
+                    <Button size='large' type='primary' onClick={handleClick} loading={isWorking}>Generate</Button>
                 </Col>
             </Row>
+            {getGenerateResult()}
         </div>
     )
 };
