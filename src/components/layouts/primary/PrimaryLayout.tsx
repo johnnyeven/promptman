bc5097faac33f3e454d26a7f4ab12cec45122fe2
liyiwen <johnnyeven@gmail.com>
2023-04-23 09:29:07 +0800

    1

--- src/components/layouts/primary/PrimaryLayout.tsx
+++ src/components/layouts/primary/PrimaryLayout.tsx
@@ -17,15 +17,15 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="PromptMan.io is a tool specifically designed for hobbyists writing StableDiffusion Prompt. You can easily view, share, and run these prompts with one click, while providing a prompt design tool that allows you to easily create a prompt that meets your needs and share it with others." />
-                <meta name="keywords" content="prompt, stable diffusion, promptman, promptman.io" />
+                <meta name="description" content="Prompaint is a tool specifically designed for hobbyists writing StableDiffusion Prompt. You can easily view, share, and run these prompts with one click, while providing a prompt design tool that allows you to easily create a prompt that meets your needs and share it with others." />
+                <meta name="keywords" content="prompt, stable diffusion, promptman, Prompaint" />
                 <meta name="author" content="Johnny Even" />
-                <meta property="og:title" content="PromptMan.io" />
-                <meta property="og:description" content="PromptMan.io is a tool specifically designed for hobbyists writing StableDiffusion Prompt. You can easily view, share, and run these prompts with one click, while providing a prompt design tool that allows you to easily create a prompt that meets your needs and share it with others." />
-                <meta property="og:url" content="https://promptman.vercel.app/" />
-                <meta property="og:site_name" content="PromptMan.io" />
+                <meta property="og:title" content="Prompaint" />
+                <meta property="og:description" content="Prompaint is a tool specifically designed for hobbyists writing StableDiffusion Prompt. You can easily view, share, and run these prompts with one click, while providing a prompt design tool that allows you to easily create a prompt that meets your needs and share it with others." />
+                <meta property="og:url" content="https://www.prompaint.com/" />
+                <meta property="og:site_name" content="Prompaint" />
                 <meta property="og:type" content="website" />
-                <title>{'PromptMan.io'}</title>
+                <title>{'Prompaint'}</title>
             </Head>
             <Layout className={[styles.layout, "font-mono"]}>
                 <Header className="flex flex-col top-0 left-0 fixed w-full z-50">
