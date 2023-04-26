d714a7fed1346fa5600fabb4c4c54790d5b01a1a
liyiwen <johnnyeven@gmail.com>
2023-04-26 14:15:05 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: false,
-//     images: {
-//         domains: ['imagecache.civitai.com'],
-//     },
-//     env: {
-//         API_URL: process.env.API_URL,
-//     },
-//     async rewrites() {
-//         return [
-//             {
-//                 source: '/task-scheduler/:path*',
-//                 destination: process.env.API_URL + '/task-scheduler/:path*'
-//             }
-//         ]
-//     }
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // reactStrictMode: false,
+    // images: {
+    //     domains: ['imagecache.civitai.com'],
+    // },
+    env: {
+        API_URL: process.env.API_URL,
+    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/task-scheduler/:path*',
+    //             destination: process.env.API_URL + '/task-scheduler/:path*'
+    //         }
+    //     ]
+    // }
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
