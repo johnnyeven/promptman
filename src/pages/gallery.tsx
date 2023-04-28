d690b1434de7514491dd4ea284386e8421e40b3b
liyiwen <johnnyeven@gmail.com>
2023-04-29 01:25:04 +0800

    4

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -2,7 +2,6 @@ import { Grid } from "@arco-design/web-react";
 import { NextPageWithLayout } from "./page";
 import PrimaryLayout from "@/components/layouts/primary/PrimaryLayout";
 import ImageCard from "@/components/image_card/ImageCard";
-import { mockImageCardProps } from "@/components/image_card/ImageCard.mocks";
 import CardFilter from "@/components/card_filter/CardFilter";
 
 const Row = Grid.Row;
@@ -18,32 +17,16 @@ const Gallery: NextPageWithLayout = () => {
             </Row>
             <Row align="start" gutter={20}>
                 <Col span={6}>
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
+
                 </Col>
                 <Col span={6}>
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
+
                 </Col>
                 <Col span={6}>
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
+
                 </Col>
                 <Col span={6}>
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
-                    <ImageCard {...mockImageCardProps.new()} />
+
                 </Col>
             </Row>
         </>
