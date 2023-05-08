71ad0eeb8d739de2d9357e22e1c71cc23b0dc9d9
liyiwen <johnnyeven@gmail.com>
2023-05-08 13:55:40 +0800

    4

--- src/components/nav_menu/index.tsx
+++ src/components/nav_menu/index.tsx
@@ -3,25 +3,47 @@ import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import LocaleSwitcher from "../locale_switcher/LocaleSwitcher";
 import UserControl from "../user_control/UserControl";
+import { useRouter } from "next/router";
 
 const MenuItem = Menu.Item;
 
-export default function NavMenu({ activeIndex = "1" }) {
+export default function NavMenu() {
+    const { route } = useRouter()
     const { t } = useTranslation('common')
+    const MenuList: { [key: string]: { key: string, name: string, link: string } } = {
+        '/': {
+            key: '1',
+            name: 'navigation.home',
+            link: '/'
+        },
+        '/app': {
+            key: '2',
+            name: 'navigation.app',
+            link: '/app'
+        },
+        '/editor': {
+            key: '3',
+            name: 'navigation.editor',
+            link: '/editor'
+        },
+        '/gallery': {
+            key: '4',
+            name: 'navigation.gallery',
+            link: '/gallery'
+        }
+    }
+
+    const activatedKey = MenuList[route]?.key || '1'
+
     return (
-        <Menu mode='horizontal' theme='dark' defaultSelectedKeys={[activeIndex]} style={{ borderBottom: 'rgb(50, 50, 50) 1px solid' }}>
-            <MenuItem key='1'>
-                <Link href={'/'}>{t('navigation.home')}</Link>
-            </MenuItem>
-            <MenuItem key='2'>
-                <Link href={'/app'}>{t('navigation.app')}</Link>
-            </MenuItem>
-            <MenuItem key='3'>
-                <Link href={'/editor'}>{t('navigation.editor')}</Link>
-            </MenuItem>
-            <MenuItem key='4'>
-                <Link href={'/gallery'}>{t('navigation.gallery')}</Link>
-            </MenuItem>
+        <Menu mode='horizontal' theme='dark' defaultSelectedKeys={[activatedKey]} style={{ borderBottom: 'rgb(50, 50, 50) 1px solid' }}>
+            {
+                Object.values(MenuList).map((item) => (
+                    <MenuItem key={item.key}>
+                        <Link href={item.link}>{t(item.name)}</Link>
+                    </MenuItem>
+                ))
+            }
             <LocaleSwitcher />
             <UserControl />
         </Menu>
